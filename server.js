e73db20b6a19c03181a879dfac2dc0c00e5deb23
Ajit Shukla <official.aj.shukla@gmail.com>
2024-12-29 01:05:15 +0530

    Updated configs

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
+const cors = require('cors');
 const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
 const connectDB = require('./config/database');
 connectDB();
+
+// Allow all origins (you can restrict it to specific origins later)
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
